7b177acf34a1914cfc613a1d6ee71282ed6719fa
NGOMNGOM <takongbaza@gmail.com>
2023-06-08 20:55:38 +0700

    Stateless, Center, and Text styling

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,11 +1,27 @@
 import "package:flutter/material.dart";
 
 void main() {
-  var app = MaterialApp(
-      title: "My Application",
-      theme: ThemeData(primarySwatch: Colors.blueGrey),
-      home: Scaffold(
-          appBar: AppBar(title: const Text("NGOMNGOM")),
-          body: const Text("This is body")));
+  var app = const MyApp();
   runApp(app);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "My Application",
+        theme: ThemeData(primarySwatch: Colors.blueGrey),
+        home: Scaffold(
+            appBar: AppBar(title: const Text("NGOMNGOM")),
+            body: const Center(
+                child: Text(
+              "This is centered",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.lightBlueAccent,
+              ),
+            ))));
+  }
 }
