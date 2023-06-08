b77610347d79570570498e847d5f7b00f3aa5530
NGOMNGOM <takongbaza@gmail.com>
2023-06-08 22:50:47 +0700

    Widget in List

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -28,29 +28,25 @@ class _IndexState extends State<Index> {
   int count = 0;
   @override
   Widget build(BuildContext context) {
+    List<Widget> data = [];
+    data.add(const Text("This is centered",
+        style: TextStyle(
+          fontSize: 20,
+          color: Colors.lightBlueAccent,
+        )));
+    data.add(Text("$count"));
+    data.add(const Image(
+        image: NetworkImage(
+            "https://cdn.britannica.com/26/162626-050-3534626F/Koala.jpg")));
     return Scaffold(
       appBar: AppBar(title: const Text("NGOMNGOM")),
       body: Center(
           child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-            const Text(
-              "This is centered",
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.lightBlueAccent,
-              ),
-            ),
-            Text("$count"),
-            const Image(
-                image: NetworkImage(
-                    "https://cdn.britannica.com/26/162626-050-3534626F/Koala.jpg"))
-          ])),
+              mainAxisAlignment: MainAxisAlignment.start, children: data)),
       floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() => count++);
-          },
-          child: const Icon(Icons.explicit_sharp)),
+          onPressed: countUp, child: const Icon(Icons.explicit_sharp)),
     );
   }
+
+  void countUp() => setState(() => count++);
 }
