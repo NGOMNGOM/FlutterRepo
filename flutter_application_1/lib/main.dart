9e88bbbcb6af514b6516420c68f12a86aa6e2cb0
NGOMNGOM <takongbaza@gmail.com>
2023-06-08 15:30:55 +0700

    Add theme

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -3,6 +3,7 @@ import "package:flutter/material.dart";
 void main() {
   var app = MaterialApp(
       title: "My Application",
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
       home: Scaffold(
           appBar: AppBar(title: const Text("NGOMNGOM")),
           body: const Text("This is body")));
