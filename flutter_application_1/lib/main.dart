e4bf26be8f22613b47162dc9f8b0e5a2ef1571c1
NGOMNGOM <takongbaza@gmail.com>
2023-06-08 15:14:50 +0700

    Scaffold

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,10 @@
 import "package:flutter/material.dart";
 
 void main() {
-  var app = const MaterialApp(title: "My Application", home: Text("Hello"));
+  var app = MaterialApp(
+      title: "My Application",
+      home: Scaffold(
+          appBar: AppBar(title: const Text("NGOMNGOM")),
+          body: const Text("This is body")));
   runApp(app);
 }
